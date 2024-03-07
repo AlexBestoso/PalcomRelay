b78843f78179794cc61769a2dbd9a7872a259bdf
Morning Star <alexbestoso@gmail.com>
2024-03-07 17:01:32 -0500

    Created main configuration page.

--- classes/webPages/mainPage.h
+++ classes/webPages/mainPage.h
@@ -15,7 +15,41 @@ class MainPage : public WebPage{
 					pc += "<input name=\"login\" type=\"submit\" value=\"login\" />";
 				pc += "</form>";
 			}else{
-				pc += "<h1>AUTHENTICATED</h1>";
+				pc += "<h1>Palcom Relay Control Panel</h1>";
+				pc += "<table>";
+				pc += "<tr>";
+					// top bar
+					pc += "<td><a href=\"logout.pal\">logout</a></td>";
+				pc += "</tr>";
+				pc += "<tr>";
+					// main section
+					pc += "<td>";
+						// user / pass update form
+						pc += "<h2>Update User</h2><br>";
+						pc += "<form action=\"updateUser.pal\" method=\"POST\" >";
+							pc += "<label>Username: </label><input name=\"username\" value=\"";pc += authFile.relayUsername; pc += "\" type=\"text\"/><br />";
+							pc += "<label>Current Password: </label><input name=\"currentPassword\" value=\"\" type=\"password\" /><br />";
+							pc += "<label>New Password: </label><input name=\"newPassword\" value=\"\" type=\"password\" /><br />";
+							pc += "<label>Confirm Password: </label><input name=\"confPassword\" value=\"\" type=\"password\" /><br />";
+							pc += "<input type=\"submit\" value=\"Update\" name=\"updateUser\" />";
+						pc += "</form> <br><hr /><br />";
+
+						// relay configuration form
+						pc += "<h2>Relay Config</h2><br>";
+						pc += "<form action=\"updateConfig.pal\" method=\"POST\">";
+							pc += "<label>Relay IP: </label><input name=\"relayIP\" type=\"text\" value=\"\" /><br>";
+							pc += "<label>Relay Port: </label><input name=\"relayPort\" type=\"text\" value=\"\" /></br>";
+							pc += "<input type=\"submit\" value=\"Update\" name=\"updateRelay\" />";
+						pc += "</form><br /><hr /><br />";
+
+						pc += "<h2>Factory Reset</h2>";
+						pc += "<form action=\"reset.pal\" method=\"POST\">";
+							pc += "<label>Admin Password: </label><input type=\"password\" name=\"password\" value=\"\" /><br />";
+							pc += "<input type=\"submit\" value=\"Reset\" name=\"factoryReset\" />";
+						pc += "</form>";
+					pc += "</td>";
+				pc += "</tr>";
+				pc += "</table>";
 			}
                         pc += "</body></html>";
 			return pc;
